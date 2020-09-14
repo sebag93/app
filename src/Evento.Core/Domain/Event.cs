4e0c61676bd357643eae2cb6cea437e0190232d7
Sebastian Guzdek <sebag93@gmail.com>
2020-09-14 13:12:21 +0200

    2.5 Model wydarzenia

---
+++ src/Evento.Core/Domain/Event.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Evento.Core.Domain
+{
+    public  class Event : Entity
+    {
+        private ISet<Ticket> _ticket = new HashSet<Ticket>();
+
+        public string Name { get; protected set; }
+        public string Description { get; protected set; }
+        public DateTime CreatedAt { get; protected set; }
+        public DateTime StartDate { get; protected set; }
+        public DateTime EndDate { get; protected set; }
+        public DateTime UpdatedAt { get; protected set; }
+        public IEnumerable<Ticket> Tickets { get; protected set; }
+
+        protected Event()
+        {
+
+        }
+
+        public Event(Guid id, string name, string descrition, DateTime startDate, DateTime endDate)
+        {
+            Id = id;
+            Name = name;
+            Description = descrition;
+            StartDate = startDate;
+            EndDate = endDate;
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public void AddTickets (int amount, decimal price)
+        {
+            for (var i = 0; i < amount; i++)
+            {
+                _ticket.Add(new Ticket());
+            }
+        }
+    }
+}
