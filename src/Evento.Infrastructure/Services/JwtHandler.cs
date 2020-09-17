9696162d674f9570caebdcfe5a83c9bdd9551cc9
Sebastian Guzdek <sebag93@gmail.com>
2020-09-17 18:36:03 +0200

    7. Infrastruktura i framework 7.1 Finalne poprawki domeny

--- src/Evento.Infrastructure/Services/JwtHandler.cs
+++ src/Evento.Infrastructure/Services/JwtHandler.cs
@@ -43,7 +43,7 @@ namespace Evento.Infrastructure.Services
             {
                 Token = token,
                 Expires = expires.ToTimestamp()
-            }
+            };
         }
     }
 }
